2e80abfb63b5e39288f7f8aa1d3551d6edc64520
dylankmz <dylankarapekmez@icloud.com>
2020-01-09 20:49:20 +0100

    exam prep exercises

---
+++ src/be/oo/Lamp.java
@@ -0,0 +1,32 @@
+package be.oo;
+
+public class Lamp {
+
+    private String merk;
+    private int vermogen;
+
+    public Lamp(String merk, int vermogen) {
+
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public int getVermogen() {
+        return vermogen;
+    }
+
+    public void setVermogen(int vermogen) {
+        this.vermogen = vermogen;
+    }
+
+    @Override
+    public String toString() {
+        return "Kenmerken van de lamp: " + merk + '-' + vermogen + "Watt";
+    }
+}
---
+++ src/be/oo/Main.java
@@ -0,0 +1,12 @@
+package be.oo;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        Lamp l1 = new Lamp("Osram", 30);
+
+        System.out.println(l1);
+    }
+
+}
