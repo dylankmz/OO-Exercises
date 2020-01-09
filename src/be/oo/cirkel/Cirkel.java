193ed0b3202d24e338818cb152576219e82d1a14
dylankmz <dylankarapekmez@icloud.com>
2020-01-09 22:37:47 +0100

    start cirkel exercise

---
+++ src/be/oo/cirkel/Cirkel.java
@@ -0,0 +1,48 @@
+package be.oo.cirkel;
+
+public class Cirkel {
+
+   private double straal;
+   private String kleur;
+
+    public Cirkel() {
+    }
+
+    public Cirkel(String kleur) {
+        this.kleur = kleur;
+    }
+
+    public Cirkel(double straal, String kleur) {
+        this.straal = straal;
+        this.kleur = kleur;
+    }
+
+    public double getStraal() {
+        return straal;
+    }
+
+    public void setStraal(double straal) {
+        this.straal = straal;
+    }
+
+    public String getKleur() {
+        return kleur;
+    }
+
+    public void setKleur(String kleur) {
+        this.kleur = kleur;
+    }
+
+    @Override
+    public String toString() {
+        return "Cirkel{" + "straal=" + straal + ", kleur='" + kleur + '\'' + '}';
+    }
+
+    public void geefOmtrek() {
+        this.straal = 2 * Math.PI * straal;
+    }
+
+    public double geefOppervlakte(double straal) {
+        return Math.PI * straal * straal;
+    }
+}
---
+++ src/be/oo/cirkel/Main.java
@@ -0,0 +1,12 @@
+package be.oo.cirkel;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Cirkel c1 = new Cirkel();
+        Cirkel c2 = new Cirkel();
+
+
+    }
+}
